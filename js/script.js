a0251fedcbe966ddc4ad7e2f6cb351714c4a84de
Alnaya <a.alnaya@yandex.ru>
2021-11-28 14:45:54 +0400

    Доделал адаптивность

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// const btns = document.querySelectorAll('.navbar__item')
+
+// for (let i = 0; i < btns.length; i++) {
+//     let element = btns[i]
+//     element.addEventListener('click', function  () {
+//         btns.forEach(item => {
+//             item.classList.remove('navbar__item_active')
+//         }) 
+//         element.classList.add('navbar__item_active')
+//     })
+// }
+
+// const messageOne = 'hello'
+// alert(messageOne)
+
+
+// let num = prompt("Введите число")
+// alert(14 <= num && num <= 91 ? "Правильная цифра" : "Не правильная цифра")
+
+// let num = prompt("Введите логин")
+// let pass
+// alert("админ" == num ? ((pass = prompt("Введите пароль")) == 1234 ? "Правильно" : pass ? "Я вас не знаю" : "Отмена") : num ? 'Я вас не знаю' : "Отменено")
+
+// alert("Добрый день")
+// let name = prompt("Назовите свое имя")
+// if (name == null) alert("У вас нет имени?")
+// let age = prompt(`${name}, сколько вам лет`)
+// if (age >= 18) {
+//     let delo = prompt("Как ваши дела? Плохо или хорошо?")
+//     if (delo == "Плохо" || delo == "плохо") {
+//         let anecd = prompt("Рассказать вам анекдот?")
+//         if (anecd == "Да" || anecd == "да") {
+//             alert("'Смешная шутка'")
+//         } else {
+//             alert("Хорошщего дня")
+//         }
+//     } else {
+//         let secret = prompt("Рассказать вам секрет?")
+//         if (secret == "Да" || secret == "да") {
+//             alert("Я прячу от кота колбасу")
+//         } else {
+//             alert("Хорошего дня")
+//         }
+//     }
+// } else {
+//     let delo = prompt("Как твои дела? Плохо или хорошо?")
+//     if (delo == "Плохо" || delo == "плохо") {
+//         let anecd = prompt("Рассказать тебе анекдот?")
+//         if (anecd == "Да" || anecd == "да") {
+//             alert("'Смешная шутка'")
+//         } else {
+//             alert("Хорошщего дня")
+//         }
+//     } else {
+//         let secret = prompt("Рассказать тебе секрет?")
+//         if (secret == "Да" || secret == "да") {
+//             alert("Я прячу от кота колбасу")
+//         } else {
+//             alert("Хорошего дня")
+//         }
+//     }
+// }
